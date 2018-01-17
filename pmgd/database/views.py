4b4ec31042d69afd283276f02f462d001f3d3d7d
yduanBioinfo <duanyou@outlook.com>
2018-01-17 21:38:35 +0800

    Add Home class in database/view.py .

--- pmgd/database/views.py
+++ pmgd/database/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.views import View
 
 # Create your views here.
+
+class Home(View):
+    def get(self,request):
+        return render_to_response("database/index.html")
