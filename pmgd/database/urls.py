517aecb176f3931207d62dcee40345e75f1f51b3
yduanBioinfo <duanyou@outlook.com>
2018-01-25 22:39:19 +0800

    Add links to navigation bar. Drop down tag should have none link.

--- pmgd/database/urls.py
+++ pmgd/database/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name = 'database'
 urlpatterns = [
     path('',views.Home.as_view(),name="home"),
+    path('Abrowse',views.Genome_browser.as_view(),name="genome_browser"),
+    path('Tools',views.Tools.as_view(),name="tools"),
+    path('Tutorial',views.Tutorial.as_view(),name="tutorial"),
+    path('Upload',views.Upload.as_view(),name="upload"),
 ]
--- pmgd/database/views.py
+++ pmgd/database/views.py
@@ -6,3 +6,20 @@ from django.views import View
 class Home(View):
     def get(self,request):
         return render_to_response("database/index.html")
+
+class Genome_browser(View):
+    def get(self,request):
+        return render_to_response("database/genome_browser.html")
+
+class Tools(View):
+    def get(self,request):
+        return render_to_response("database/tools.html")
+
+class Tutorial(View):
+    def get(self,request):
+        return render_to_response("database/tutorial.html")
+
+class Upload(View):
+    def get(self,request):
+        return render_to_response("database/upload.html")
+
