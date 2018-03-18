419966184bd246072b350f97c24a2b5ea62cfa8b
yduanBioinfo <duanyou@outlook.com>
2018-03-18 21:36:45 +0800

    Add view test in tests.py. And this test class should be seperated into more specialized class for each page.

--- pmgd/database/tests.py
+++ pmgd/database/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.test import Client
+
+client = Client()
 
 # Create your tests here.
+
+class HomeViewTests(TestCase):
+    def test_view_on_about(self):
+        '''
+        About page.
+        '''
+        response = self.client.get(reverse('database:home'))
+        self.assertEqual(response.status_code,200)
+
+    def test_view_on_genome_browser(self):
+        response = self.client.get(reverse('database:genome_browser'))
+        self.assertEqual(response.status_code,200)
+
+    def test_view_on_tools(self):
+        response = self.client.get(reverse('database:tools'))
+        self.assertEqual(response.status_code,200)
+    
+    def test_view_on_tools(self):
+        response = self.client.get(reverse('database:tutorial'))
+        self.assertEqual(response.status_code,200)
+
+    def test_view_on_tools(self):
+        response = self.client.get(reverse('database:upload'))
+        self.assertEqual(response.status_code,200)
+
